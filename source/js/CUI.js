1b7c5aa3174cbbf25dab8c25c0f034da6b5b20c6
Larry Davis <lawdavis@adobe.com>
2012-09-13 00:41:05 -0700

    Added namespace docs, debug option

--- source/js/CUI.js
+++ source/js/CUI.js
@@ -1,2 +1,13 @@
-// Base namespace
+/** 
+ * The main CUI namespace.
+ *	
+ * @namespace
+ *
+ * @property {Object} options           Main options for CloudUI components.
+ * @property {Boolean} options.debug    If true, show debug messages for all components.
+*/
 var CUI = {};
+
+CUI.options = {
+  debug: false
+};
