0310bff07ce7a3a57afe8ae21cc326e2bfe9874f
Larry Davis <lawdavis@adobe.com>
2012-09-21 16:43:58 -0700

    CUI utility functions

---
+++ source/js/CUI.Util.js
@@ -0,0 +1,7 @@
+CUI.util = {};
+
+CUI.util.getDataTarget = function($element) {
+  var href = $element.attr('href');
+  var $target = $($element.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))); // strip for ie7
+  return $target;
+};
