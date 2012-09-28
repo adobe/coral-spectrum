65bd02ac2e15f92eb7bca242e14ae975821331df
Larry Davis <lawdavis@adobe.com>
2012-09-27 23:16:35 -0700

    Added examples to docs

--- source/js/components/CUI.Alert.js
+++ source/js/components/CUI.Alert.js
@@ -4,15 +4,64 @@
     extend: CUI.Widget,
     
     /**
-      Alert dialog
+      @extends CUI.Widget
+      @classdesc An optionally closable alert message.
       
+      <div class="alert error">
+        <button class="close" data-dismiss="alert">&times;</button>
+        <strong>ERROR</strong><div>Uh oh, something went wrong with the whozit!</div>
+      </div>
+      
+      @example
+<caption>Instantiate with Class</caption>
+var alert = new CUI.Alert({
+  element: '#myAlert',
+  heading: 'ERROR',
+  content: 'An error has occurred.',
+  closable: true
+});
+
+// Hide the alert, change the content, then show it again
+alert.hide().set({ content: 'Another error has occurred.'}).show();
+
+// jQuery style works as well
+$('#myAlert').alert('hide');
+
+      @example
+<caption>Instantiate with jQuery</caption>
+$('#myAlert').alert({
+  heading: 'ERROR',
+  content: 'An error has occurred.',
+  closable: true
+});
+
+// Hide the alert, change the content, then show it again
+$('#myAlert').alert('hide').alert({ heading: 'Another error has occurred.'}).alert('show');
+
+// A reference to the element's alert instance is stored as data-alert
+var alert = $('#myAlert').data('alert');
+alert.hide();
+
+      @example
+<caption>Data API: Hide alert</caption>
+<description>When an element within the alert has <code><span class="atn">data-dismiss</span>=<span class="atv">"alert"</span></code>, it will hide the alert.</description>
+&lt;a data-dismiss=&quot;alert&quot;&gt;Dismiss&lt;/a&gt;
+
+      @example
+<caption>Markup</caption>
+&lt;div class=&quot;alert error&quot;&gt;
+  &lt;button class=&quot;close&quot; data-dismiss=&quot;alert&quot;&gt;&amp;times;&lt;/button&gt;
+  &lt;strong&gt;ERROR&lt;/strong&gt;&lt;div&gt;Uh oh, something went wrong with the whozit!&lt;/div&gt;
+&lt;/div&gt;
+
+      @desc Creates a new alert
       @constructs
       
-      @param {Object} options                   Component options
-      @param {String} options.heading           Title of the alert (HTML)
-      @param {String} options.content           Content of the alert (HTML)
-      @param {Boolean} options.closable         Array of button descriptors
-      @param {String} [options.type=default]    Type of Alert to display. One of error, notice, success, help, or info
+      @param {Object} options                               Component options
+      @param {String} [options.heading=Type, capitalized]   Title of the alert (HTML)
+      @param {String} options.content                       Content of the alert (HTML)
+      @param {Boolean} options.closable                     Array of button descriptors
+      @param {String} [options.type=error]                  Type of Alert to display. One of error, notice, success, help, or info
     */
     construct: function(options) {
       // Catch clicks to dismiss alert
