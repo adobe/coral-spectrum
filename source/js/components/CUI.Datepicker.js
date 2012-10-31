7ff18f0e41951d857328891ef668c78e6e3da485
Bastian Frank <bastian.frank@pidoco.com>
2012-10-31 16:32:35 +0100

    CUI-96 Second version of CUI.Datepicker

--- source/js/components/CUI.Datepicker.js
+++ source/js/components/CUI.Datepicker.js
@@ -27,8 +27,13 @@
                 timeout = null;
                 return;
             }
-            this.displayDate = new Date(this.$element.find("input").val()); 
+            this.displayDate = this.options.selectedDate = new Date(this.$element.find("input").val()); 
             this._showPicker();
+        }.bind(this));
+        
+        this.$element.on("change", "input", function() {
+            this.displayDate = this.options.selectedDate = new Date(this.$element.find("input").val());
+            this._renderCalendar();
         }.bind(this));
         
         this.$element.on("blur", "input", function() {
@@ -37,30 +42,35 @@
                 this._hidePicker();
             }.bind(this), 200);
             
-        }.bind(this));
-        
-        
+        }.bind(this));  
     },
     
     defaults: {
         monthNames: null,
         dayNames: null,
         format: null,
-        currentDate: new Date(),
-        startDay: 1
+        selectedDate: new Date(),
+        startDay: 0
     },
     
     displayDate: null,
+    pickerShown: false,
+    
+    _keyPress: function() {
+        if (!this.pickerShown) return;
+        
+        // TODO: Keyboard actions
+    },
     
     _showPicker: function() {
         this._renderCalendar();
         this.$element.find(".datepicker-calendar").show();
-        
+        this.pickerShown = true;
     },
     
     _hidePicker: function() {
         this.$element.find(".datepicker-calendar").hide();
-    
+        this.pickerShown = false;
     },
     
     _addMissingElements: function() {
@@ -70,25 +80,31 @@
     },
     
     _renderCalendar: function() {
-        if (this.displayDate === null) this.displayDate = this.options.currentDate;
-        
+        if (isNaN(this.displayDate.getFullYear())) {
+            this.displayDate = new Date();
+        }
+        if (this.options.selectedDate && isNaN(this.options.selectedDate.getFullYear())) {
+            this.options.selectedDate = null;
+        }
         var displayYear = this.displayDate.getFullYear();
         var displayMonth = this.displayDate.getMonth() + 1;
         
         var monthName = this.options.monthNames[displayMonth - 1];
         var title = $('<div class="popover-heading">' + monthName + " " + displayYear + '</div>');
         
-        // Month selection
-        var nextMonthElement = $("<span>&gt;</span>").on("click", function() {
+        // Month selection        
+        var nextMonthElement = $("<button class=\"next-month\">›</button>").on("mousedown", function(event) {
+            event.preventDefault();
             this.displayDate = new Date(displayYear, displayMonth - 1 + 1, 1);
             this._renderCalendar();
         }.bind(this));
-        var prevMonthElement = $("<span>&lt;</span>").on("click", function() {
+        var prevMonthElement = $("<button class=\"prev-month\">‹</button>").on("mousedown", function(event) {
+            event.preventDefault();
             this.displayDate = new Date(displayYear, displayMonth - 1 - 1, 1);
             this._renderCalendar();
         }.bind(this));
         
-        title.append(prevMonthElement).append(nextMonthElement);
+        title.append(nextMonthElement).append(prevMonthElement);
         
         var $calendar = this.$element.find(".datepicker-calendar");
         $calendar.empty().append(title);
@@ -100,7 +116,7 @@
         for(var i = 0; i < 7; i++) {
             day = (i + this.options.startDay) % 7;
             var dayName = this.options.dayNames[day];
-            html += "<td>" + dayName.substr(0, 3) + "</td>";
+            html += "<td><span>" + dayName + "</span></td>";
         }
         html += "</tr>";
         table.append("<thead>" + html + "</thead>");
@@ -116,27 +132,71 @@
             for(var d = 0; d < 7; d++) {
                 day = (w * 7 + d) - monthStartsAt + 1;
                 displayDate = new Date(displayYear, displayMonth - 1, day);
-                var cssClass = (displayDate.getMonth() + 1 !== displayMonth) ? "class=\"dimmed\"" : "";
-                html += "<td " + cssClass + "><a href=\"#\" data-date=\"" + this._formatDate(displayDate) + "\">" + displayDate.getDate() + "</a></td>";
+                var isCurrentMonth = (displayDate.getMonth() + 1) === displayMonth;
+                var cssClass = "";
+                if (this._compareDates(displayDate, this.options.selectedDate) === 0) cssClass += " day-selected";
+                if (cssClass) cssClass = "class=\"" + cssClass + "\"";
+                if (isCurrentMonth) {
+                    html += "<td " + cssClass + "><a href=\"#\" data-date=\"" + this._formatDate(displayDate) + "\">" + displayDate.getDate() + "</a></td>";            
+                } else {
+                    html += "<td " + cssClass + "><span>" + displayDate.getDate() + "</span></td>";
+                }
             }
             html +="</tr>";
         }
         table.append("<tbody>" + html + "</tbody>");
-        table.on("click", "a", function(event) {
+        table.on("mousedown", "a", function(event) {
+            event.preventDefault();
             this._gotoDate($(event.target).data("date"));
         }.bind(this));
         $calendar.append(table);
         
     },
     
-    _gotoDate: function(dateString) {
-        this.$element.find("input").val(dateString);
+    _compareDates: function(d1, d2) {
+        if (!d1 || !d2) return false;
+        if (d1.getFullYear() < d2.getFullYear()) return -1;
+        if (d1.getFullYear() > d2.getFullYear()) return 1;
+        if (d1.getMonth() < d2.getMonth()) return -1;
+        if (d1.getMonth() > d2.getMonth()) return 1;
+        if (d1.getDate() < d2.getDate()) return -1;
+        if (d1.getDate() > d2.getDate()) return 1;
+        return 0;
     },
     
-    _formatDate: function(date) {
-        var d = new Date();
-        return (date.getFullYear() + "-" + this._pad(date.getMonth() + 1) + "-" + this._pad(date.getDate()));
+    _gotoDate: function(dateString) {
+        this.$element.find("input").val(dateString);
+        this.options.selectedDate = this.displayDate = new Date(dateString);
+        this._renderCalendar();
     },
+    
+    _formatDate: function(date) {        
+        var rfn = {
+            d: function(date) { return this._pad(date.getDate()); },
+            j: function(date) { return date.getDate(); },
+            w: function(date) { return date.getDay(); },
+            m: function(date) { return this._pad(date.getMonth() + 1); },
+            n: function(date) { return date.getMonth() + 1; },
+            Y: function(date) { return date.getFullYear(); },
+            y: function(date) { return ('' + date.getFullYear()).substr(2); },
+            c: function(date) { return date.format("Y-m-d"); }
+           
+        };
+        var fmt = this.options.format;
+        var result = "";
+        for(var i = 0; i < fmt.length; i++) {
+            var t = fmt.charAt(i);
+            if (rfn[t]) {
+                result += (rfn[t].bind(this))(date);
+            } else {
+                result += t;
+            }
+        }
+        
+        return result;
+    },
+    
+    
     _pad: function(s) {
         if (s < 10) return "0" + s;
         return s;
@@ -145,7 +205,7 @@
   });
   
   CUI.Datepicker.monthNames = ["January", "February", "March", "April", "May", "Juni", "July", "August", "September", "October", "November", "December"];
-  CUI.Datepicker.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  CUI.Datepicker.dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   CUI.Datepicker.format = "Y-m-d";
 
   CUI.util.plugClass(CUI.Datepicker);
