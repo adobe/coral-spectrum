ccd78666f05bca2d8da8053c1b77faec8ac3f36e
Bastian Frank <git@bastian-frank.de>
2013-03-19 10:51:42 +0100

    CUI-426 Finished initial Tooltip widget implementation and added test.

---
+++ source/test/test.CUI.Tooltip.js
@@ -0,0 +1,72 @@
+describe('CUI.Tooltip', function() {
+  
+  // A default target element
+  var target = $("<div>");
+  
+  // A config we can reuse
+  var tooltipConfig = {
+    content: 'TestContent',
+    interactive: true,
+    delay: 10,
+    arrow: "right",
+    target: target
+  };
+  
+
+  
+  
+  describe('definition', function() {
+    // 'definition' block is a temporary workaround for kmiyashiro/grunt-mocha/issues/22
+    it('should be defined in CUI namespace', function() {
+      expect(CUI).to.have.property('Tooltip');
+    });
+
+    it('should be defined on jQuery object', function() {
+      var div = $('<div/>');
+      expect(div).to.have.property('tooltip');
+    });
+  });
+  
+  describe('tooltip from plugin', function() {
+    var el = $('<div/>').tooltip(tooltipConfig);
+  
+    it('should have correct CSS classnames', function() {
+      expect(el).to.have.class('tooltip');
+      expect(el).to.have.class('arrow-right');
+      expect(el).to.have.class('hidden');  // Hidden on default            
+    });
+  });
+  
+  describe('tooltip from markup', function() {
+    var tooltipHTML = [
+      '<div class="tooltip arrow-left" data-interactive="true">',
+      'Tooltip',
+      '</div>'
+    ].join();
+    
+    var el = $(tooltipHTML);
+    
+    var tooltip = new CUI.Tooltip({
+      element: el,
+      target: target
+    });
+  
+    it('should have correct CSS classnames', function() {
+      expect(el).to.have.class('tooltip');
+      expect(el).to.have.class('arrow-left');
+      expect(el).to.have.class('hidden');  // Hidden on default         
+    });
+  });
+  
+  describe('options', function() {
+    it('can set content with class', function() {
+      var el = $('<div/>');
+      var modal = new CUI.Tooltip({
+        element: el
+      });
+      modal.set('content', 'TestContent');
+      expect(el).to.have.html('TestContent');
+    });
+  });
+  
+});
