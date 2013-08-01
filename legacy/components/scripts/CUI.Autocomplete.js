e0d26ad539485be7f63f7e691d27aab4ce19d270
Damien Antipa <dantipa@adobe.com>
2013-08-02 00:19:30 +0200

    CUI-822 Rework and move "autocomplete" component out of legacy (merge filters/tags/combobox)

--- legacy/components/scripts/CUI.Autocomplete.js
+++ legacy/components/scripts/CUI.Autocomplete.js
@@ -1,74 +1,117 @@
-;(function($) {
-  "use strict";
+(function ($, window, undefined) {
+    CUI.Autocomplete = new Class(/** @lends CUI.Autocomplete# */{
+        toString: 'Autocomplete',
 
-  CUI.Autocomplete = new Class(/** @lends CUI.Autocomplete# */{
-    toString: "Autocomplete",
-    extend: CUI.Widget,
+        extend: CUI.Widget,
 
-    defaults: {
-    },
+        defaults: {
+            showClearButton: true,
+            showSuggestions: true,
+            showTags: true
+        },
 
-    construct: function() {
-      this._input = this.$element.children("input");
-      this._setupValues();
-      this._setupClear();
-    },
+        construct: function () {
+            var self = this;
 
-    _setupValues: function() {
-      var name = this._input.prop("name");
+            // find elements
+            this._input = this.$element.children('input');
+            this._suggestions = this.$element.find('.selectlist');
+            this._tags = this.$element.find('.taglist');
 
-      this.$element.children("ol.autocomplete-values")
-        .on("click tap", ".autocomplete-remove", function(e) {
-          e.preventDefault();
-          $(this).closest("li").remove();
-        })
-        .children("li").each(function() {
-          var el = $(this);
-          el.prepend("<button class='autocomplete-remove icon-close xsmall' />");
+            // create additional objects
+            this._clearBtn = $('<button/>', {
+                'class': 'autocomplete-clear icon-close'
+            }).fipo('tap', 'click', function (event) {
+                event.preventDefault();
 
-          var hidden = $("<input type='hidden'>")
-            .val(el.data("value"));
+                self.clear();
+                self._input.focus();
+            });
 
-          if (name) {
-            hidden.prop("name", name);
-          }
+            // apply
+            this.applyOptions(); 
+        },
 
-          el.append(hidden);
+        applyOptions: function () {
+            this._setClearButton();
+            this._setSuggestions();
+            this._setTags();
+        },
+
+        _setClearButton: function () {
+            if (this.options.showClearButton) {
+
+                this._clearBtn.appendTo(this.$element);
+                this._input.on('keyup.clearBtn', this._refreshClear.bind(this));
+                this._refreshClear();
+
+            } else {
+
+                this._clearBtn.detach();
+                this._input.off('keyup.clearBtn');
+
+            }
+        },
+
+        _setSuggestions: function () {
+            if (this.options.showSuggestions) {
+
+                // if the element is not there, create it
+                if (this._suggestions.length === 0) {
+                    this._suggestions = $('<ul/>', {
+                        'class': 'selectlist'
+                    }).appendTo(this.$element);
+                }
+
+                this._selectList = new CUI.SelectList({
+                    element: this._suggestions,
+                    relatedElement: this._input
+                });
+
+                // TODO add opening link
+            } else {
+                // remove opening link
+            }
+        },
+
+        _setTags: function () {
+            if (this.options.showTags) {
+
+                // if the element is not there, create it
+                if (this._tags.length === 0) {
+                    this._tags = $('<ul/>', {
+                        'class': 'taglist'
+                    }).appendTo(this.$element);
+                }
+
+                this._tagList = new CUI.TagList({
+                    element: this._tags
+                });
+
+                // TODO add link
+            } else {
+                // remove tag view
+                // remove link
+            }
+        },
+
+        _refreshClear: function () {
+            this._clearBtn.toggleClass('hide', this._input.val().length === 0);
+        },
+
+        clear: function () {
+            this._input.val('');
+            this._refreshClear();
+        }
+    });
+
+    CUI.util.plugClass(CUI.Autocomplete);
+
+    // Data API
+    if (CUI.options.dataAPI) {
+        $(document).on('cui-contentloaded.data-api', function (e) {
+            $('[data-init~=autocomplete]', e.target).autocomplete();
         });
-    },
-
-    _setupClear: function() {
-      var self = this;
-
-      this._clear = $("<button class='autocomplete-clear icon-close xsmall' />")
-        .on("click tap", function(e) {
-          e.preventDefault();
-          self.clear();
-          self._input.focus();
-        });
-
-      this._refreshClear();
-      this._clear.appendTo(this.$element);
-
-      this._input.on("input", function() {
-        self._refreshClear();
-      });
-    },
-
-    _refreshClear: function() {
-      this._clear.toggleClass("hide", this._input.val().length === 0);
-    },
-
-    clear: function() {
-      this._input.val("");
-      this._refreshClear();
     }
-  });
 
-  CUI.util.plugClass(CUI.Autocomplete);
-
-  // Data API
-  $(document).on("cui-contentloaded.data-api", function(e) {
-    $("[data-init~='autocomplete']", e.target).autocomplete();
-  });
-}(window.jQuery));
+}(jQuery, this));
