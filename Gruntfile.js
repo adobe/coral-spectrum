e81e651c71520f77d225df1e2e707e2a79595443
brownlee <brownlee@adobe.com>
2013-10-08 14:53:28 -0700

    CUI-1025 deprecation warnings for Handlebars

--- Gruntfile.js
+++ Gruntfile.js
@@ -561,8 +561,7 @@ module.exports = function (grunt) {
                 },
                 files: {
                     '<%= dirs.build %>/js/CUI.Templates.js': [
-                    '<%= dirs.legacy %>/components/templates/*.hbs',
-                    '<%= dirs.components %>/**/templates/*.hbs'
+                    '<%= dirs.legacy %>/components/templates/*.hbs'
                     ]
                 }
             }
