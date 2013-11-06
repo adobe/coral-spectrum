93b9f64dba5df19b17f24f53458e5168034ce6fe
Damien Antipa <dantipa@adobe.com>
2013-11-06 16:12:42 +0100

    @trivial remove private functions from jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -619,7 +619,8 @@ module.exports = function (grunt) {
                 src: ['<%= dirs.temp %>/js/**.js', '<%= dirs.temp %>/js/components/**.js'],
                 options: {
                     destination: '<%= dirs.build %>/doc',
-                    template: 'res/docTemplate/'
+                    template: 'res/docTemplate/',
+                    private: false
                 }
             }
         }, // jsdoc
