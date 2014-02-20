70f433df38c7c056780a8365aef7ffca1aeaf546
Damien Antipa <dantipa@adobe.com>
2014-02-20 13:46:49 +0100

    @trivial add nib to stylus config

--- grunt_tasks/options/stylus.js
+++ grunt_tasks/options/stylus.js
@@ -5,7 +5,8 @@ module.exports = {
       urlfunc: 'embedurl',
       paths: [
         './<%= dirs.modules %>/'
-      ]
+      ],
+      import: ['nib'],
     },
     files: {
       '<%= dirs.build %>/<%= dirs.css %>/coral.css': './<%= dirs.temp %>/index.styl'
