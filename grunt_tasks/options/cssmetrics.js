77d27222b55c3433e8ce124d7ffe46109edbb29a
Daniel Wabyick <dwabyick@adobe.com>
2014-03-13 15:30:53 -0700

    CUI-2075 css metrics

---
+++ grunt_tasks/options/cssmetrics.js
@@ -0,0 +1,12 @@
+// Grunt Configuration: CSSMetrics
+module.exports = {
+  coralui: {
+    src: [
+      '<%= dirs.build %>/css/coral.css',
+      '<%= dirs.build %>/css/coral.min.css'
+    ],
+    options: {
+      maxSelectors: 4096
+    }
+  }
+}
