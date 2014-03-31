78ab530e911fd745c02fa0cafb8b53700389b051
Larry Davis <lawdavis@adobe.com>
2014-03-31 15:03:24 -0700

    CUI-1208/CUI-2118 Configure Karma for running tests and generating coverage reports for all Coral components

--- grunt_tasks/options/coralui-testrunner.js
+++ grunt_tasks/options/coralui-testrunner.js
@@ -1,3 +1,32 @@
+var pkg = require('../../package.json');
+var path = require('path');
+
+// Build a list of files for Karma
+// We use a custom list of files here to include source files/tests for all Coral components
+var includeOrder = pkg.coral.order.scripts.map(function(script) {
+  return path.join('node_modules', script);
+});
+
+// Start with the basic dependencies
+var karmaFiles = [
+  'build/js/libs/jquery-message.js',
+  'build/js/libs/jquery-validator.js',
+  'build/js/libs/moment.js',
+  'build/js/libs/toe.js',
+];
+
+// Add in the include order from package.json
+karmaFiles = karmaFiles.concat(includeOrder);
+
+// Add in the actual tests
+karmaFiles.push('build/tests/*.js');
+
 module.exports = {
-  package: '<%= package %>'
-}
+  options: {
+    files: karmaFiles,
+    preprocessors: {
+      // Instrument the source files we'll be running tests against
+      'node_modules/coral*/**/*.js': ['coverage']
+    }
+  }
+};
