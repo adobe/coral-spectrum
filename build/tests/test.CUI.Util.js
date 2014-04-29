89bd725431ed3c97e363903688123ca9645f8dd7
Gilles Knobloch <gknob@adobe.com>
2014-04-29 12:02:10 +0200

    @releng - Prepare for releasing [2.3.0] (RTC not needed)

---
+++ build/tests/test.CUI.Util.js
@@ -0,0 +1,25 @@
+describe('CUI.Util', function() {
+  
+  describe('capitalize()', function() {
+    it("should capitalize lowercase string", function() {
+      expect(CUI.util.capitalize("testString")).to.equal("TestString");
+    });
+    
+    it("should not break already capitalized string", function() {
+      expect(CUI.util.capitalize("TestString")).to.equal("TestString");
+    });
+  });
+  
+  describe('decapitalize()', function() {
+    it("should decapitalize uppercase string", function() {
+      expect(CUI.util.decapitalize("TestString")).to.equal("testString");
+    });
+    
+    it("should not break already decapitalized string", function() {
+      expect(CUI.util.decapitalize("testString")).to.equal("testString");
+    });
+  });
+
+  // TODO: test $.fn.loadWithSpinner
+  // TODO: test CUI.util.plugClass
+});
