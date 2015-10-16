7ec6bb91437893b80c14ccdf25d9155c73813a17
Frank Karstens <karstens@adobe.com>
2015-10-16 17:44:44 +0200

    @trivial remove unused option for generating docs

--- grunt_tasks/options/generate-docs-mapping.js
+++ grunt_tasks/options/generate-docs-mapping.js
@@ -1,5 +1,4 @@
 module.exports = {
-  coralModules: '<%= package.devDependencies %>',
   moduleDataPath: '{{coralModulePath}}/package.json',
   outputFile: '<%= dirs.build %>/<%= dirs.documentation %>/documentation-mapping.json'
 };
