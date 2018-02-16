8043122e49daf37dd270998023d5aee0b7bd1c3b
Stephan Ringel <ringel@adobe.com>
2018-02-16 16:15:48 +0100

    trivial - fix copyright year

--- coralui-externals/polyfills/focus-ring.js
+++ coralui-externals/polyfills/focus-ring.js
@@ -1,7 +1,7 @@
 /*
  * ADOBE CONFIDENTIAL
  *
- * Copyright 2019 Adobe Systems Incorporated
+ * Copyright 2018 Adobe Systems Incorporated
  * All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
