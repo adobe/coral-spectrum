0b6db8d16dbcb88a7366a9c4d0b9493e6ed5c401
Stephan Ringel <ringel@adobe.com>
2018-03-20 10:30:28 +0100

    CORAL-2 Rethink export to window mechanism
    * fixed playground exports

--- coralui-component-playground/index.js
+++ coralui-component-playground/index.js
@@ -1,13 +1,13 @@
 // Import all components
-import '../index';
-
-import '/coralui-theme-spectrum';
-import '/coralui-externals';
+import * as components from '../index';
 
 import Playground from './src/scripts/Playground';
 import './src/styles/index.css';
 
 // Expose component on the Coral namespace
 window.customElements.define('coral-playground', Playground);
+window.Coral.Playground = Playground;
 
-export {Playground};
+for (const component in components) {
+  window.Coral[component] = components[component];
+}
