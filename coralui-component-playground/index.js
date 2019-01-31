a0f843ebf209f7094b1d839e74b0b06d26ef1a5d
Stephan Ringel <ringel@adobe.com>
2019-01-31 18:36:29 +0100

    CORAL-0 Sync with coral-cloudui
    * fix Playground

--- coralui-component-playground/index.js
+++ coralui-component-playground/index.js
@@ -4,10 +4,11 @@ import * as components from '../index';
 import Playground from './src/scripts/Playground';
 import './src/styles/index.css';
 
-// Expose component on the Coral namespace
 window.customElements.define('coral-playground', Playground);
-window.Coral.Playground = Playground;
 
+// Export all on the Coral namespace
+window.Coral = {};
+window.Coral.Playground = Playground;
 for (const component in components) {
   window.Coral[component] = components[component];
 }
