db5efc400dd59918d79f430981901799721cc72e
icaraps <icaraps@hotmail.com>
2019-09-12 12:10:38 +0200

    Switch: fix readOnly behavior

--- coral-component-switch/src/scripts/Switch.js
+++ coral-component-switch/src/scripts/Switch.js
@@ -178,7 +178,8 @@ class Switch extends BaseFormField(BaseComponent(HTMLElement)) {
     this._readOnly = transform.booleanAttr(value);
     this._reflectAttribute('readonly', this._readOnly);
     
-    this._elements.input.disabled = this._readOnly;
+    this.classList.toggle('is-readOnly', this._readOnly);
+    this._elements.input.tabIndex = this._readOnly ? -1 : 0;
   }
   
   /*
