90276e4c0c6f375b8a071f658df56e793d3d5782
Neetika Jain <neejain@adobe.com>
2020-02-07 10:08:00 +0530

    CUI-7337 buttons change months should have specific labels

--- coral-component-calendar/i18n/translations.js
+++ coral-component-calendar/i18n/translations.js
@@ -16,132 +16,170 @@ export default {
     "Next": "Next",
     "Today, {0} selected/[translation hint:date]": "Today, {0} selected",
     "Today, {0}/[translation hint:date]": "Today, {0}",
-    "{0} selected/[translation hint:date]": "{0} selected"
+    "{0} selected/[translation hint:date]": "{0} selected",
+    "Previous Month":"Previous Month",
+    "Next Month":"Next Month"
   },
   "fr-FR": {
     "Previous": "Précédent",
     "Next": "Suivant",
     "Today, {0} selected/[translation hint:date]": "Aujourd’hui, sélection de {0}",
     "Today, {0}/[translation hint:date]": "Aujourd’hui, {0}",
-    "{0} selected/[translation hint:date]": "Sélection de {0}"
+    "{0} selected/[translation hint:date]": "Sélection de {0}",
+    "Previous Month":"Mois précédent",
+    "Next Month":"Mois suivant"
   },
   "de-DE": {
     "Previous": "Zurück",
     "Next": "Weiter",
     "Today, {0} selected/[translation hint:date]": "Heute, {0} ausgewählt",
     "Today, {0}/[translation hint:date]": "Heute, {0}",
-    "{0} selected/[translation hint:date]": "{0} ausgewählt"
+    "{0} selected/[translation hint:date]": "{0} ausgewählt",
+    "Previous Month":"Vorheriger Monat",
+    "Next Month":"Nächster Monat"
   },
   "it-IT": {
     "Previous": "Precedente",
     "Next": "Successivo",
     "Today, {0} selected/[translation hint:date]": "Oggi, {0} selezionati",
     "Today, {0}/[translation hint:date]": "Oggi, {0}",
-    "{0} selected/[translation hint:date]": "{0} selezionati"
+    "{0} selected/[translation hint:date]": "{0} selezionati",
+    "Previous Month":"Mese precedente",
+    "Next Month":"Mese successivo"
   },
   "es-ES": {
     "Previous": "Anterior",
     "Next": "Siguiente",
     "Today, {0} selected/[translation hint:date]": "Hoy, {0} seleccionados",
     "Today, {0}/[translation hint:date]": "Hoy, {0}",
-    "{0} selected/[translation hint:date]": "{0} seleccionados"
+    "{0} selected/[translation hint:date]": "{0} seleccionados",
+    "Previous Month":"Mes anterior",
+    "Next Month":"Mes siguiente"
   },
   "pt-BR": {
     "Previous": "Anterior",
     "Next": "Próximo",
     "Today, {0} selected/[translation hint:date]": "Hoje, {0} selecionados",
     "Today, {0}/[translation hint:date]": "Hoje, {0}",
-    "{0} selected/[translation hint:date]": "{0} selecionados"
+    "{0} selected/[translation hint:date]": "{0} selecionados",
+    "Previous Month":"Mês Anterior",
+    "Next Month":"Mês Seguinte"
   },
   "ja-JP": {
     "Previous": "前へ",
     "Next": "次へ",
     "Today, {0} selected/[translation hint:date]": "今日、{0} が選択されました",
     "Today, {0}/[translation hint:date]": "今日、{0}",
-    "{0} selected/[translation hint:date]": "{0} が選択されました"
+    "{0} selected/[translation hint:date]": "{0} が選択されました",
+    "Previous Month":"前月",
+    "Next Month":"翌月"
   },
   "ko-KR": {
     "Previous": "이전으로",
     "Next": "다음",
     "Today, {0} selected/[translation hint:date]": "현재 {0}이(가) 선택됨",
     "Today, {0}/[translation hint:date]": "현재 {0}",
-    "{0} selected/[translation hint:date]": "{0}이(가) 선택됨"
+    "{0} selected/[translation hint:date]": "{0}이(가) 선택됨",
+    "Previous Month":"지난 달",
+    "Next Month":"다음 달"
   },
   "zh-CN": {
     "Previous": "上一步",
     "Next": "下一步",
     "Today, {0} selected/[translation hint:date]": "已选定今天 {0}",
     "Today, {0}/[translation hint:date]": "今天 {0}",
-    "{0} selected/[translation hint:date]": "已选定 {0}"
+    "{0} selected/[translation hint:date]": "已选定 {0}",
+    "Previous Month":"上个月",
+    "Next Month":"下个月"
   },
   "zh-TW": {
     "Previous": "上一頁",
     "Next": "下一頁",
     "Today, {0} selected/[translation hint:date]": "今天，已選取 {0}",
     "Today, {0}/[translation hint:date]": "今天，{0}",
-    "{0} selected/[translation hint:date]": "已選取 {0} "
+    "{0} selected/[translation hint:date]": "已選取 {0} ",
+    "Previous Month":"上個月",
+    "Next Month":"下個月"
   },
   "nl-NL": {
     "Previous": "Vorige",
     "Next": "Volgende",
     "Today, {0} selected/[translation hint:date]": "Vandaag {0} geselecteerd",
     "Today, {0}/[translation hint:date]": "Vandaag {0} ",
-    "{0} selected/[translation hint:date]": "{0} geselecteerd"
+    "{0} selected/[translation hint:date]": "{0} geselecteerd",
+    "Previous Month":"Vorige maand",
+    "Next Month":"Volgende maand"
   },
   "da-DK": {
     "Previous": "Foregående",
     "Next": "Næste",
     "Today, {0} selected/[translation hint:date]": "I dag, {0} valgt",
     "Today, {0}/[translation hint:date]": "I dag, {0} ",
-    "{0} selected/[translation hint:date]": "{0} valgt"
+    "{0} selected/[translation hint:date]": "{0} valgt",
+    "Previous Month":"Sidste måned",
+    "Next Month":"Næste måned"
   },
   "fi-FI": {
     "Previous": "Edellinen",
     "Next": "Seuraava",
     "Today, {0} selected/[translation hint:date]": "Tänään, {0} valittu",
     "Today, {0}/[translation hint:date]": "Tänään, {0}",
-    "{0} selected/[translation hint:date]": "{0} valittu"
+    "{0} selected/[translation hint:date]": "{0} valittu",
+    "Previous Month":"Edellinen kuukausi",
+    "Next Month":"Seuraava kuukausi"
   },
   "nb-NO": {
     "Previous": "Forrige",
     "Next": "Neste",
     "Today, {0} selected/[translation hint:date]": "I dag, {0} valgt",
     "Today, {0}/[translation hint:date]": "I dag, {0}",
-    "{0} selected/[translation hint:date]": "{0} valgt"
+    "{0} selected/[translation hint:date]": "{0} valgt",
+    "Previous Month":"Forrige måned",
+    "Next Month":"Neste måned"
   },
   "sv-SE": {
     "Previous": "Föregående",
     "Next": "Nästa",
     "Today, {0} selected/[translation hint:date]": "Idag, {0} vald",
     "Today, {0}/[translation hint:date]": "Idag, {0}",
-    "{0} selected/[translation hint:date]": "{0} vald"
+    "{0} selected/[translation hint:date]": "{0} vald",
+    "Previous Month":"Föregående månad",
+    "Next Month":"Nästa månad"
   },
   "cs-CZ": {
     "Previous": "Předchozí",
     "Next": "Další",
     "Today, {0} selected/[translation hint:date]": "Dnes je vybráno: {0}",
     "Today, {0}/[translation hint:date]": "Dnes, {0}",
-    "{0} selected/[translation hint:date]": "Vybráno: {0}"
+    "{0} selected/[translation hint:date]": "Vybráno: {0}",
+    "Previous Month":"Předchozí měsíc",
+    "Next Month":"Příští měsíc"
   },
   "pl-PL": {
     "Previous": "Wstecz",
     "Next": "Dalej",
     "Today, {0} selected/[translation hint:date]": "Dziś, wybrano {0}",
     "Today, {0}/[translation hint:date]": "Dziś, {0}",
-    "{0} selected/[translation hint:date]": "wybrano {0}"
+    "{0} selected/[translation hint:date]": "wybrano {0}",
+    "Previous Month":"Poprzedni miesiąc",
+    "Next Month":"Następny miesiąc"
   },
   "ru-RU": {
     "Previous": "Предыдущий",
     "Next": "Следующий",
     "Today, {0} selected/[translation hint:date]": "Сегодня, {0}, выбрано",
     "Today, {0}/[translation hint:date]": "Сегодня, {0}",
-    "{0} selected/[translation hint:date]": "{0} выбрано"
+    "{0} selected/[translation hint:date]": "{0} выбрано",
+    "Previous Month":"Предыдущий месяц",
+    "Next Month":"Следующий месяц"
   },
   "tr-TR": {
     "Previous": "Önceki",
     "Next": "İleri",
     "Today, {0} selected/[translation hint:date]": "Bugün, {0} seçildi",
     "Today, {0}/[translation hint:date]": "Bugün, {0}",
-    "{0} selected/[translation hint:date]": "{0} seçildi"
+    "{0} selected/[translation hint:date]": "{0} seçildi",
+    "Previous Month":"Önceki Ay",
+    "Next Month":"Gelecek Ay"
   }
 };
