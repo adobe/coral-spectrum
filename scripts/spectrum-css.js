4678f76a35d151ecfb98d907c115feeab7a1cce4
icaraps <icaraps@hotmail.com>
2020-04-20 13:39:07 +0200

    Add script to fix spectrum-css invalid CSS

---
+++ scripts/spectrum-css.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+// Removes invalid CSS see https://github.com/adobe/spectrum-css/issues/656
+
+const fs = require('fs');
+const barLoader = 'node_modules/@adobe/spectrum-css/dist/components/barloader/index-diff.css';
+const coachmark = 'node_modules/@adobe/spectrum-css/dist/components/coachmark/index-diff.css';
+
+const removeLines = (file, lines) => {
+  const content = fs.readFileSync(file, 'utf8');
+  fs.writeFileSync(file, content.split('\n').slice(lines).join('\n'), 'utf8');
+};
+
+removeLines(barLoader, 8);
+removeLines(coachmark, 31);
