e10ea91e9a5d472376242e607dae43132af2aad4
Michael Jordan <mijordan@adobe.com>
2020-04-27 16:40:59 -0400

    CQ-4291308 Port to Spectrum: Column View: the checkboxes in the grid all lack programmatic names (AEM Sites)

---
+++ coral-component-columnview/i18n/translations.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export default {
+  "en-US": {
+    ", checked": ", checked",
+    ", unchecked": ", unchecked"
+  },
+  "de-DE": {
+    ", checked": ", markiert",
+    ", unchecked": ", nicht markiert"
+  },
+  "fr-FR": {
+    ", checked": ", cochée",
+    ", unchecked": ", pas cochée"
+  },
+  "it-IT": {
+    ", checked": ", selezionata",
+    ", unchecked": ", non selezionata"
+  },
+  "ja-JP": {
+    ", checked": "、チェック",
+    ", unchecked": "、未チェック"
+  },
+  "es-ES": {
+    ", checked": ", marcada",
+    ", unchecked": ", desmarcada"
+  },
+  "ko-KR": {
+    ", checked": ", 선택됨",
+    ", unchecked": ", 선택되지 않은"
+  },
+  "zh-CN": {
+    ", checked": "，选中",
+    ", unchecked": "，未选中"
+  },
+  "zh-TW": {
+    ", checked": "，選中",
+    ", unchecked": "，未選中"
+  },
+  "pt-BR": {
+    ", checked": ", marcada",
+    ", unchecked": ", desmarcada"
+  },
+  "nl-NL": {
+    ", checked": ", geselecteerd",
+    ", unchecked": ", neit geselecteerd"
+  },
+  "da-DK": {
+    ", checked": ", valgte",
+    ", unchecked": ", ikke valgt"
+  },
+  "fi-FI": {
+    ", checked": ", valittu",
+    ", unchecked": ", valittuna"
+  },
+  "nb-NO": {
+    ", checked": ", valgt",
+    ", unchecked": ", ikke valgt"
+  },
+  "sv-SE": {
+    ", checked": ", markerad",
+    ", unchecked": ", avmarkerad"
+  },
+  "cs-CZ": {
+    ", checked": ", vybráno",
+    ", unchecked": ", není vybráno"
+  },
+  "pl-PL": {
+    ", checked": ", zaznaczone",
+    ", unchecked": ", nie zaznaczone"
+  },
+  "ru-RU": {
+    ", checked": ", выбранный",
+    ", unchecked": ", неотобранный"
+  },
+  "tr-TR": {
+    ", checked": ", seçilmiş",
+    ", unchecked": ", seçilmemiş"
+  }
+}
