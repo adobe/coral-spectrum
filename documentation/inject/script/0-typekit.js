ff16d7da6d48b3e3792958dee5bfaaa8aafe9f6c
CezCz <czarek.czernecki@gmail.com>
2020-11-12 14:30:36 +0000

    Deploying to gh-pages from  @ d55091912754abdc8e801ee1da427744c4ee69b0 🚀

---
+++ documentation/inject/script/0-typekit.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+(function() {
+  var typeKitId = 'ruf7eed';
+  
+  if (window.Coral && window.Coral.options && window.Coral.options.typeKitId) {
+    typeKitId = window.Coral.options.typeKitId;
+  }
+  
+  var config = {
+    kitId: typeKitId,
+    scriptTimeout: 3000
+  };
+  
+  if (!window.Typekit) { // we load the typescript only once
+    var h = document.getElementsByTagName("html")[0];
+    h.className += " wf-loading";
+    var t = setTimeout(function() {
+      h.className = h.className.replace(/(\s|^)wf-loading(\s|$)/g, " ");
+      h.className += " wf-inactive";
+    }, config.scriptTimeout);
+    var tk = document.createElement("script"),
+      d = false;
+    
+    // Always load over https
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js'
+    tk.type = "text/javascript";
+    tk.async = "true";
+    tk.onload = tk.onreadystatechange = function() {
+      var a = this.readyState;
+      if (d || a && a !== "complete" && a !== "loaded") {
+        return;
+      }
+      d = true;
+      clearTimeout(t);
+      try {
+        Typekit.load(config);
+      } catch (b) {}
+    };
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(tk, s);
+  }
+}());
