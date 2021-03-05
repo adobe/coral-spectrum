a2726a4f39ff3ab540d90b9be01a6e6203c6f780
ramasubr <ramasubr@adobe.com>
2021-03-05 18:02:43 +0530

    fix: adding a selectlist based solution switcher
    With AEM branding changes now the switcher nneds to be updated as the unified shell this is an
    implementation for the same

---
+++ coral-component-shell/src/scripts/ShellSelectListSwitcher.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import {BaseComponent} from '../../../coral-base-component';
+import {SelectableCollection} from '../../../coral-collection';
+import selectListSwitcher from '../templates/selectListSwitcher';
+import {SelectList} from '../../../coral-component-list';
+
+const CLASSNAME = '_coral-Shell-selectListSwitcher';
+
+/**
+ @class Coral.Shell.SelectListSwitcher
+ @classdesc A Shell SelectList Switcher component
+ @htmltag coral-shell-selectlistswitcher
+ @extends {HTMLElement}
+ @extends {BaseComponent}
+ */
+class ShellSelectListSwitcher extends BaseComponent(HTMLElement) {
+  /** @ignore */
+  constructor() {
+    super();
+
+    // Template
+    this._elements = {};
+    selectListSwitcher.call(this._elements);
+
+    // Listen for mutations
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        for (let i = 0 ; i < mutation.addedNodes.length ; i++) {
+          const addedNode = mutation.addedNodes[i];
+          // Move non secondary solutions to the container
+          if (addedNode.nodeName === 'CORAL-SELECTLIST' && !addedNode.hasAttribute('secondary')) {
+            this._elements.container.appendChild(addedNode);
+          }
+        }
+      });
+    });
+
+    observer.observe(this, {
+      // Only care about direct children
+      childList: true
+    });
+  }
+
+  /**
+   The item collection.
+
+   @type {Collection}
+   @readonly
+   */
+  get items() {
+    // Construct the collection on first request
+    if (!this._items) {
+      this._items = new Collection({
+        host: this,
+        itemTagName: 'coral-selectlist',
+        onItemAdded: this._validateSelection,
+        onItemRemoved: this._validateSelection
+      });
+    }
+
+    return this._items;
+  }
+
+  /** @ignore */
+  render() {
+    super.render();
+
+    this.classList.add(CLASSNAME);
+
+
+    const container = this.querySelector('._coral-Shell-selectList-container') || this._elements.container;
+
+    // Remove it so we can process solutions
+    if (container.parentNode) {
+      container.remove();
+    }
+
+    // Move non secondary solutions to the container
+    Array.prototype.forEach.call(this.querySelectorAll('coral-selectlist:not([secondary])'), (item) => {
+      container.appendChild(item);
+    });
+
+    // Put the container as first child
+    this.insertBefore(container, this.firstChild);
+  }
+}
+
+export default ShellSelectListSwitcher;
