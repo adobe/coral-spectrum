0fffa1b450ddc3ec5789e909d20a5593f4f2f24c
icaraps <icaraps@hotmail.com>
2021-04-21 13:34:45 +0200

    feat: Remove polyfills

--- coral-externals/index.js
+++ coral-externals/index.js
@@ -11,19 +11,19 @@
  */
 
 // @IE
-import './polyfills/element.closest.js';
-import './polyfills/element.matches.js';
-import './polyfills/element.remove.js';
-import './polyfills/classList-partial-shim.js';
-import './polyfills/CustomEvent.js';
-import './polyfills/focus-options-polyfill.js';
-import 'core-js/es/reflect';
-import 'core-js/es/promise';
+// import './polyfills/element.closest.js';
+// import './polyfills/element.matches.js';
+// import './polyfills/element.remove.js';
+// import './polyfills/classList-partial-shim.js';
+// import './polyfills/CustomEvent.js';
+// import './polyfills/focus-options-polyfill.js';
+// import 'core-js/es/reflect';
+// import 'core-js/es/promise';
 // Accessibility
 import '@adobe/focus-ring-polyfill';
 
 // @IE Force custom elements polyfill to work with ES5
-import './polyfills/forcePolyfill.js';
+// import './polyfills/forcePolyfill.js';
 // Using custom polyfill built from https://github.com/joeldenning/custom-elements/tree/builtin instead.
 // customElements.enableCustomizedBuiltins is set to true by default here.
-import '@ungap/custom-elements';
+// import '@ungap/custom-elements';
