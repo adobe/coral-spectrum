86707a94e74d71e71f6921960efa878150574f8b
hparwani2 <himanshu.parwani8@gmail.com>
2022-11-27 15:20:11 +0530

    checking debug logs

--- coral-gulp/configs/karma.conf.js
+++ coral-gulp/configs/karma.conf.js
@@ -142,7 +142,7 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
     // Don't fail if no tests
     failOnEmptyTestSuite: false,
