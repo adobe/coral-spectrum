10293b2a5a7556ebae24fe131be210ec7a66aa65
Pareesh Gupta <pareeshgupta@gmail.com>
2022-12-01 15:44:45 +0530

    fix: Correct build (#266)
    Co-authored-by: Pareesh Gupta <paregupt@pareeshs-mbp.corp.adobe.com>

--- coral-gulp/configs/karma.conf.js
+++ coral-gulp/configs/karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+    browsers: ['ChromeHeadless'/*, 'FirefoxHeadless'*/],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
