89e2f6f89486c60fe2b783076c02c65bd2c52061
RazvanPRA <71783091+RazvanPRA@users.noreply.github.com>
2023-01-31 06:44:53 +0000

    Deploying to gh-pages from @ adobe/coral-spectrum@c54bff00478d9ba3e4d488bd175d0156d6ebb535 🚀

--- js/coral.js
+++ js/coral.js
@@ -85195,7 +85195,7 @@ var Coral = (function (exports) {
 
   var name = "@adobe/coral-spectrum";
   var description = "Coral Spectrum is a JavaScript library of Web Components following Spectrum design patterns.";
-  var version$1 = "4.15.9";
+  var version$1 = "4.15.10";
   var homepage = "https://github.com/adobe/coral-spectrum#readme";
   var license = "Apache-2.0";
   var repository = {
