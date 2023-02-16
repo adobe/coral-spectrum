859a6ee3507ec771e499c390b6418678ffcbb8df
Alexandru Gheorghe <ens18702@adobe.com>
2023-02-16 16:35:24 +0200

    fix(CQ-4273759): Added aria-label to multifield add new item button based on PR comments

--- coral-component-multifield/src/scripts/Multifield.js
+++ coral-component-multifield/src/scripts/Multifield.js
@@ -752,7 +752,7 @@ const Multifield = Decorator(class extends BaseComponent(HTMLElement) {
     this._handleRoleList();
 
     // a11y Add aria-label to the add button to give context to screen reader users
-    $("[coral-multifield-add]").attr("aria-label","Add a new item to the options list");
+    document.querySelector('[coral-multifield-add]').setAttribute("aria-label","Add");
 
     // Assign the content zones, moving them into place in the process
     this.template = this._elements.template;
