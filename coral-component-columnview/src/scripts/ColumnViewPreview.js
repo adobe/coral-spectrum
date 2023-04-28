ed1891ba235a1801a2b8819f548aad887a19fd3a
Tudor Craita <tudorc@ensemble.com>
2023-04-28 14:18:06 +0300

    Update ColumnViewPreview.js

--- coral-component-columnview/src/scripts/ColumnViewPreview.js
+++ coral-component-columnview/src/scripts/ColumnViewPreview.js
@@ -96,20 +96,13 @@ const ColumnViewPreview = Decorator(class extends BaseComponent(HTMLElement) {
     let element;
     let elementLabel;
 
-    // @a11y If the previous column has selected items,
-    // do not include item values in the tab order,
-    // so that a keyboard user can quickly advance to a subsequent toolbar.
-    const tabIndex = (this.parentElement &&
-      this.parentElement.tagName === 'CORAL-COLUMNVIEW' &&
-      this.parentElement.selectedItems.length) ? -1 : 0;
-
     for (i = 0 ; i < length ; i++) {
       element = elements[i];
       elementLabel = element.previousElementSibling;
       elementLabel.id = elementLabel.id || commons.getUID();
       element.setAttribute('aria-labelledby', elementLabel.id);
       element.setAttribute('role', 'textbox');
-      element.setAttribute('tabindex', tabIndex);
+      element.setAttribute('tabindex', 0);
       element.setAttribute('aria-readonly', 'true');
 
       // force ChromeVox to read value of textbox
