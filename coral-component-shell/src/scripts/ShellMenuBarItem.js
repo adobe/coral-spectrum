e20a85e840aa91c08307b4687521aa6d06b004d0
StoianLucian <101817370+StoianLucian@users.noreply.github.com>
2024-03-25 14:12:26 +0200

    -

--- coral-component-shell/src/scripts/ShellMenuBarItem.js
+++ coral-component-shell/src/scripts/ShellMenuBarItem.js
@@ -423,7 +423,7 @@ class ShellMenuBarItem extends BaseComponent(HTMLElement) {
       let shellMenuButton = this._elements.shellMenuButton;
       let shellMenuButtonIcon = shellMenuButton.getElementsByTagName('coral-icon')[0];
 
-      if(shellMenuButtonIcon !== null) {
+      if(shellMenuButtonIcon !== undefined) {
         shellMenuButtonIcon.setAttribute('alt', this.title);
       } 
     });
