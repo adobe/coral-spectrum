2d0ebff2678bb12c98d37d6de16100462f1f3d10
Pareesh <pareeshgupta@gmail.com>
2024-04-02 11:34:09 +0000

    Deploying to gh-pages from @ adobe/coral-spectrum@d545314710fe8ad516f95f4ff61bd0d12e950bcb 🚀

--- playground/dist/js/coral.js
+++ playground/dist/js/coral.js
@@ -85231,7 +85231,7 @@
 
   var name = "@adobe/coral-spectrum";
   var description = "Coral Spectrum is a JavaScript library of Web Components following Spectrum design patterns.";
-  var version$1 = "4.15.28";
+  var version$1 = "4.15.29";
   var homepage = "https://github.com/adobe/coral-spectrum#readme";
   var license = "Apache-2.0";
   var repository = {
