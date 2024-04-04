33d082371593721d9546c3b217cdf1e2168911d7
ens13533 <ens13533@adobe.com>
2024-04-04 17:46:02 +0300

    added i18n for aria-label attribute

--- coral-component-colorinput/src/scripts/ColorInput.js
+++ coral-component-colorinput/src/scripts/ColorInput.js
@@ -688,7 +688,7 @@ const ColorInput = Decorator(class extends BaseFormField(BaseComponent(HTMLEleme
       this.showSwatches === showSwatches.ON) {
       this._elements.colorPreview.label.textContent = i18n.get('Swatches');
       if (this.previousSibling) {
-        this.previousSibling.setAttribute('aria-label', 'Confirm hex value');
+        this.previousSibling.setAttribute('aria-label', i18n.get('Confirm hex value'));
       }
       this._elements.overlay.setAttribute('aria-label', i18n.get('Swatches'));
     } else {
