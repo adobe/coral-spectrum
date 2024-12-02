385b881c391b30efe0b8ba1a1866379c3fb3f779
Marian Giurge <ens13533@adobe.com>
2024-12-02 11:37:30 +0200

    SITES-27153 added alt attribute

--- coral-component-card/src/scripts/Card.js
+++ coral-component-card/src/scripts/Card.js
@@ -378,7 +378,9 @@ const Card = Decorator(class extends BaseComponent(HTMLElement) {
     const imagesCount = images.length;
     for (let i = 0 ; i < imagesCount ; i++) {
       const image = images[i];
-      image.setAttribute("alt", "");
+      if (!image.hasAttribute("alt")) {
+        image.setAttribute("alt", "");
+      }
       if (!image.complete) {
         image.classList.add('is-loading');
       }
