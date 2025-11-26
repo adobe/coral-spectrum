295037a160bcf1c653c0e9ea0d753b2c96c4c6d6
Lucian Stoian <lucians@ENSRO0414.local>
2025-11-26 10:49:00 +0200

    fix: SITES-37686 improve coral-list

--- coral-base-list/src/scripts/BaseList.js
+++ coral-base-list/src/scripts/BaseList.js
@@ -11,7 +11,7 @@
  */
 
 import {SelectableCollection} from '../../../coral-collection';
-import {transform, validate} from '../../../coral-utils';
+import {commons, transform, validate} from '../../../coral-utils';
 
 const CLASSNAME = '_coral-Menu';
 
@@ -130,6 +130,7 @@ const BaseList = (superClass) => class extends superClass {
     }
 
     const items = this._getSelectableItems();
+    items.forEach((item, i) => this.setIndex(item, i === 0 ? 0 : -1));
     items[0].focus();
   }
 
@@ -140,6 +141,9 @@ const BaseList = (superClass) => class extends superClass {
     }
 
     const items = this._getSelectableItems();
+    items.forEach((item, i) =>
+      this.setIndex(item, i === items.length - 1 ? 0 : -1)
+    );
     items[items.length - 1].focus();
   }
 
@@ -159,8 +163,11 @@ const BaseList = (superClass) => class extends superClass {
     }
 
     if (index < items.length - 1) {
+      this.setIndex(target, -1);
+      this.setIndex(items[index + 1], 0);
       items[index + 1].focus();
     } else {
+      items.forEach((item, i) => this.setIndex(item, i === 0 ? 0 : -1));
       items[0].focus();
     }
   }
@@ -181,8 +188,13 @@ const BaseList = (superClass) => class extends superClass {
     }
 
     if (index > 0) {
+      this.setIndex(target, -1);
+      this.setIndex(items[index - 1], 0);
       items[index - 1].focus();
     } else {
+      items.forEach((item, i) =>
+        this.setIndex(item, i === items[length - 1] ? 0 : -1)
+      );
       items[items.length - 1].focus();
     }
   }
@@ -200,6 +212,12 @@ const BaseList = (superClass) => class extends superClass {
       if (items.length > 0) {
         items[0].focus();
       }
+    }
+  }
+
+  setIndex(element, value) {
+    if (element instanceof HTMLElement) {
+      element.setAttribute("tabindex", value);
     }
   }
 
@@ -227,6 +245,13 @@ const BaseList = (superClass) => class extends superClass {
     if (!this._interaction) {
       this.interaction = interaction.ON;
     }
+
+    commons.nextFrame(() => {
+      const children = this.childNodes;
+      if (this.interaction === interaction.ON && children.length > 0) {
+        children.forEach((child, i) => this.setIndex(child, i === 0 ? 0 : -1));
+      }
+    });
   }
 };
 
