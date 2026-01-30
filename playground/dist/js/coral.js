84b1fcfc057f34d9940e565f950e2c8a54596d22
StoianLucian <101817370+StoianLucian@users.noreply.github.com>
2026-01-30 12:56:06 +0000

    Deploying to gh-pages from @ adobe/coral-spectrum@87ba64d823a53e8400140d8b36c1e36dbab5bff3 🚀

--- playground/dist/js/coral.js
+++ playground/dist/js/coral.js
@@ -86030,7 +86030,7 @@
 
   var name = "@adobe/coral-spectrum";
   var description = "Coral Spectrum is a JavaScript library of Web Components following Spectrum design patterns.";
-  var version$1 = "4.21.7";
+  var version$1 = "4.21.8";
   var homepage = "https://github.com/adobe/coral-spectrum#readme";
   var license = "Apache-2.0";
   var repository = {
