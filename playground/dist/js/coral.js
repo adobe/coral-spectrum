53c4cca0ccefb6eb936d5f5dc8b63ed2f189b199
StoianLucian <101817370+StoianLucian@users.noreply.github.com>
2026-05-19 09:16:58 +0000

    Deploying to gh-pages from @ adobe/coral-spectrum@b6068c7ceca955d5f96192ee6ec99341e791c918 🚀

--- playground/dist/js/coral.js
+++ playground/dist/js/coral.js
@@ -58610,8 +58610,6 @@
         } // The id reference for an HTML element that labels the button element accessibility name for the button element
         else if (name === 'aria-labelledby') {
             if (value || !this.getAttribute('aria-label')) {
-              this._elements.button.setAttribute('aria-labelledby', "".concat(value, " ").concat(this._elements.button.id));
-
               this._elements.overlay.setAttribute('aria-labelledby', value || this._elements.button.id);
 
               this._elements.selectList[this._hasMenuItemRadioGroup() ? 'setAttribute' : 'removeAttribute']('aria-labelledby', value || this._elements.button.id);
